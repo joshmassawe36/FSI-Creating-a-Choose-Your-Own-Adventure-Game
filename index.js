ced3543d8666c761cd7652d3e75065f170233b4f
Joshua Massawe <joshmassawe36@gmail.com>
2022-07-14 20:37:27 -0700

    for you to review.. and create your own adventure

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //Your Code here
+
+
+let response = (window.prompt("Do you go left or right?"))
+
+
+
+console.log(response)
+
+if (response === "left"){
+    console.log("user submitted left")
+} else if(response === "right") {
+    console.log("user submitted right")
+}else {
+    console.log("follow instructions")
+}
